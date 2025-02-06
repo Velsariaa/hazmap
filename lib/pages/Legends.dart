fed279bd1be0141c740b3fdbd3a0cd6cd74cdf8e
keni <kennyranes2319@gmail.com>
2025-02-06 22:12:56 +0800

    [Update] Legends

--- lib/pages/Legends.dart
+++ lib/pages/Legends.dart
@@ -33,7 +33,7 @@ class LegendsPage extends StatelessWidget {
             child: Container(
               width: MediaQuery.of(context).size.width * 0.95,
               height: MediaQuery.of(context).size.height * 0.7,
-              color: Color.fromARGB(255, 204, 204, 204).withOpacity(0.27),
+              color: const Color.fromARGB(255, 204, 204, 204).withOpacity(0.27),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
@@ -50,47 +50,232 @@ class LegendsPage extends StatelessWidget {
                     const SizedBox(height: 20),
                     Expanded(
                       child: GridView.count(
-                        shrinkWrap:
-                            true, // Ensures GridView does not exceed its parent's size
+                        shrinkWrap: true,
                         crossAxisCount: 2,
                         crossAxisSpacing: 10,
                         mainAxisSpacing: 10,
                         children: List.generate(6, (index) {
-                          return Container(
-                            color: Colors.white.withOpacity(0.8),
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Image.asset(
-                                      'lib/assets/earthquakehazard.png',
-                                      width: 50,
-                                      height: 50,
-                                    ),
-                                    const SizedBox(width: 10),
-                                    const Expanded(
-                                      // Ensures text does not overflow
-                                      child: Text(
-                                        'Earthquake Hazard',
-                                        style: TextStyle(
-                                          fontSize: 24,
-                                          fontWeight: FontWeight.bold,
+                          if (index == 1) {
+                            return Container(
+                              color: Colors.white.withOpacity(0),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 100 * 0.7,
+                                        height: 100 * 0.7,
+                                        child: Image.asset(
+                                          'lib/assets/brokenhazard.png',
                                         ),
-                                        overflow: TextOverflow
-                                            .ellipsis, // Adds "..." if text is too long
                                       ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 10),
-                                const Text(
-                                  'This is a description of the earthquake hazard.',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ],
-                            ),
-                          );
+                                      const SizedBox(width: 10),
+                                      Expanded(
+                                        child: Transform.translate(
+                                          offset: const Offset(-17,
+                                              0), 
+                                          child: const Text(
+                                            'Broken',
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'This is Broken Hazard.',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else if (index == 2) {
+                            return Container(
+                              color: Colors.white.withOpacity(0),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 50 * 0.3,
+                                        height: 50 * 0.3,
+                                        child: Image.asset(
+                                          'lib/assets/brokenhazard.png',
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      const Expanded(
+                                        child: Text(
+                                          'Flood',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'This is Flood Hazard.',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else if (index == 3) {
+                            return Container(
+                              color: Colors.white.withOpacity(0),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 50 * 0.3,
+                                        height: 50 * 0.3,
+                                        child: Image.asset(
+                                          'lib/assets/brokenhazard.png',
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      const Expanded(
+                                        child: Text(
+                                          'Fire',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'This is Fire Hazard.',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else if (index == 4) {
+                            return Container(
+                              color: Colors.white.withOpacity(0),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 50 * 0.3,
+                                        height: 50 * 0.3,
+                                        child: Image.asset(
+                                          'lib/assets/brokenhazard.png',
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      const Expanded(
+                                        child: Text(
+                                          'Landslide',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'This is Landslide Hazard.',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else if (index == 5) {
+                            return Container(
+                              color: Colors.white.withOpacity(0),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 50 * 0.3,
+                                        height: 50 * 0.3,
+                                        child: Image.asset(
+                                          'lib/assets/brokenhazard.png',
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      const Expanded(
+                                        child: Text(
+                                          'Tsunami',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'This is Tsunami Hazard.',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            );
+                          } else {
+                            return Container(
+                              color: Colors.white.withOpacity(0),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 50 * 0.3,
+                                        height: 50 * 0.3,
+                                        child: Image.asset(
+                                          'lib/assets/earthquakehazard.png',
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      const Expanded(
+                                        child: Text(
+                                          'Earthquake',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    'This is a description of the earthquake hazard.',
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
                         }),
                       ),
                     ),
